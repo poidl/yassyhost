b4af0d7322d6cdcd4d343356055bc8f6cce15b8b
poidl <hoitaus@gmail.com>
2016-06-24 16:41:39 +1000

    (not really) fix problem with name longer than 4 characters

--- src/jack_plugin.rs
+++ src/jack_plugin.rs
@@ -1,15 +1,9 @@
-#![allow(unused_imports)]
-#![allow(dead_code)]
-#![allow(unused_variables)]
-#![allow(non_snake_case)]
-
 extern crate libc;
-use std::ffi::CStr;
+// use std::ffi::CStr;
 use std::ffi::CString;
 use std::ptr;
 // use plugin;
 use midi;
-use midi::*;
 use yassy::plugin;
 use std::str;
 use jack::*;
@@ -17,35 +11,40 @@ use jack::*;
 #[repr(C)]
 pub struct jack_plugin<'a> {
     name: &'a str,
-    pub client: *mut jack_client_t,
-    pub in_port: port,
-    pub output: port,
-    plugin: plugin::SynthPlugin,
-//     pub output: *mut f32,
-    // ports: &'a [port]
+    pub client: *mut JackClientT,
+    pub in_port: Port,
+    pub output: Port,
+    plugin: plugin::SynthPlugin, /*     pub output: *mut f32,
+                                  * ports: &'a [Port] */
 }
 
-pub struct port {
-    pub handle: *mut jack_port_t,
-    pub data: *mut f32
+pub struct Port {
+    pub handle: *mut JackPortT,
+    pub data: *mut f32,
 }
 
 impl<'a> jack_plugin<'a> {
     pub fn new(hostname: &str) -> jack_plugin {
-        let mut h = jack_plugin{
+        let mut h = jack_plugin {
             name: hostname,
             client: ptr::null_mut(),
-            in_port: port{handle: ptr::null_mut(), data: ptr::null_mut() },
-            output: port{handle: ptr::null_mut(), data: ptr::null_mut() },
-            plugin: plugin::SynthPlugin::new()
+            in_port: Port {
+                handle: ptr::null_mut(),
+                data: ptr::null_mut(),
+            },
+            output: Port {
+                handle: ptr::null_mut(),
+                data: ptr::null_mut(),
+            },
+            plugin: plugin::SynthPlugin::new(),
         };
-        let jo = JackNullOption;
-        let js = JackNullStatus;
+        let jo = JACK_NULL_OPTION;
+        let js = JACK_NULL_STATUS;
         let nameptr = CString::new(h.name).unwrap().as_ptr();
         unsafe {
             h.client = jack_client_open(nameptr, jo, &js);
         }
-        if h.client == 0 as *mut jack_client_t {
+        if h.client == 0 as *mut JackClientT {
             println!("jack server not running?");
             // TODO return error
         };
@@ -55,11 +54,19 @@ impl<'a> jack_plugin<'a> {
         unsafe {
             let portname = CString::new("midi_in").unwrap().as_ptr();
             let porttype = CString::new("8 bit raw midi").unwrap().as_ptr();
-            self.in_port.handle = jack_port_register(self.client, portname, porttype, JackPortIsInput, 32768u64);
+            self.in_port.handle = jack_port_register(self.client,
+                                                     portname,
+                                                     porttype,
+                                                     JACK_PORT_IS_INPUT,
+                                                     32768u64);
 
             let portname = CString::new("audio_out").unwrap().as_ptr();
             let porttype = CString::new("32 bit float mono audio").unwrap().as_ptr();
-            self.output.handle = jack_port_register(self.client, portname, porttype, JackPortIsOutput, 32768u64);
+            self.output.handle = jack_port_register(self.client,
+                                                    portname,
+                                                    porttype,
+                                                    JACK_PORT_IS_OUTPUT,
+                                                    32768u64);
         }
     }
     pub fn midievent(&mut self, msg: &u8) {
@@ -76,66 +83,3 @@ impl<'a> jack_plugin<'a> {
         self.plugin.get_amp()
     }
 }
-
-// pub trait isJackSynthPlugin: {
-//     fn connect_port(&mut self, u32, *mut libc::c_void);
-//     fn midievent(&mut self, msg: &u8) ;
-//     fn set_fs(&mut self, f64);
-//     fn get_amp(&mut self) -> f32;
-//     fn map_params(&mut self, u32, *mut libc::c_void);
-// }
-
-//
-// #[repr(C)]
-// pub struct JackSynthPlugin {
-//     pub in_port: *mut jack_port_t,
-//     pub output: *mut f32,
-//     pub plugin: plugin::SynthPlugin,
-// }
-//
-// impl  JackSynthPlugin {
-//     pub fn new() -> JackSynthPlugin {
-//         // let np = ptr::null();
-//         let mut jackplugin = JackSynthPlugin {
-//             in_port: ptr::null_mut(),
-//             output: ptr::null_mut(),
-//             plugin: plugin::SynthPlugin::new(),
-//         };
-//         // TODO
-//         jackplugin.in_port = jackplugin.plugin.midi_in as *mut jack_port_t;
-//         jackplugin.output = jackplugin.plugin.audio_out;
-//         jackplugin
-//     }
-// }
-//
-
-// impl isLv2SynthPlugin for Lv2SynthPlugin {
-//     fn connect_port(&mut self, port: u32, data: *mut libc::c_void) {
-//         match port {
-//             0 => unsafe{self.in_port = data  as *const lv2::LV2_Atom_Sequence},
-//             1 => unsafe{self.output = data as *mut f32 },
-//             _ => self.map_params(port,data)
-//         }
-//     }
-//     fn map_params(&mut self, port: u32, data: *mut libc::c_void) {
-//         let nparams = 1;
-//         let iport = port - 2; //TODO: don't hardcode number of input/output ports
-//         if (iport <= nparams-1) {
-//             println!("connecting port: {}", port);
-//             unsafe{self.plugin.synth.params[iport as usize]= &*(data  as *mut f32) };
-//             // println!("param: {}",  *(self.synth.params[0]));
-//         } else {
-//             panic!("Not a valid PortIndex: {}", iport)
-//         }
-//     }
-//     fn midievent(&mut self, msg: &u8) {
-//         let mm = msg as midi::MidiMessage;
-//         self.plugin.midievent(mm)
-//     }
-//     fn set_fs(&mut self, fs: f64) {
-//         self.plugin.set_fs(fs);
-//     }
-//     fn get_amp(&mut self) -> f32 {
-//         self.plugin.get_amp()
-//     }
-// }
--- src/main.rs
+++ src/main.rs
@@ -9,64 +9,74 @@ mod jack;
 mod jack_plugin;
 use jack::*;
 
-use std::thread;
+use std::time::Duration;
+
+// use std::thread;
 // use websocket::{Server, Message, Sender, Receiver};
 // use websocket::message::Type;
 // use websocket::header::WebSocketProtocol;
 
-use std::ffi::CString;
-use std::ffi::CStr;
-use std::sync::{Arc, Mutex};
-use std::sync::mpsc;
-use std::ptr;
-use std::time::Duration;
+// use std::ffi::CString;
+// use std::ffi::CStr;
+// use std::sync::{Arc, Mutex};
+// use std::sync::mpsc;
+// use std::ptr;
 // use jack_plugin;
-// use yassy::plugin;
+// use yassy::Plugin;
 
 
-extern fn process(jack_nframes_t: u32, ptr: *mut libc::c_void) -> isize {
+extern "C" fn process(jack_nframes_t: u32, ptr: *mut libc::c_void) -> isize {
     unsafe {
-        let plugin = ptr as *mut plugin;
+        let plugin = ptr as *mut Plugin;
 
         let inport = &(*plugin).in_port;
         let outport = &(*plugin).output;
         let buf = jack_port_get_buffer(inport.handle, jack_nframes_t);
-        let out = jack_port_get_buffer (outport.handle, jack_nframes_t) as *mut f32;
+        let out = jack_port_get_buffer(outport.handle, jack_nframes_t) as *mut f32;
         let event_count = jack_midi_get_event_count(buf);
 
-        let mut event = JackMidiEvent{ time: 0, size: 0, buffer: std::ptr::null_mut() as *mut libc::c_uchar};
+        let mut event = JackMidiEvent {
+            time: 0,
+            size: 0,
+            buffer: std::ptr::null_mut() as *mut libc::c_uchar,
+        };
 
         let ievent = 0;
         jack_midi_event_get(&mut event, buf, ievent);
-
+        // println!("jack_frames_t: {}", jack_nframes_t);
         for i in 0..jack_nframes_t {
-            if (event.time  == i)  & (ievent < event_count) {
+            if (event.time == i) & (ievent < event_count) {
                 (*plugin).midievent(&*event.buffer);
                 jack_midi_event_get(&mut event, buf, ievent);
             }
             let amp = (*plugin).get_amp();
             *out.offset(i as isize) = amp;
         }
-
-
     }
     0
 }
 
-pub type plugin<'a> = jack_plugin::jack_plugin<'a>;
+pub type Plugin<'a> = jack_plugin::jack_plugin<'a>;
 
 fn main() {
-    let mut p = plugin::new("yassyhost");
+    unsafe {
+        let h = jack_client_name_size();
+        println!("jack_client_name_size(): {}", h);
+    }
+    let mut p = Plugin::new("yass");
     p.set_fs();
     p.connect();
-    let cbpluginptr = (&p as *const plugin) as *const libc::c_void;
-    unsafe {
-        let rval = jack_set_process_callback(p.client, process, cbpluginptr);
-        let ga = jack_activate(p.client);
-    }
-    let five = Duration::new(5, 0);
-    while true {
-        std::thread::sleep(five)
-    }
 
+    let cbpluginptr = (&p as *const Plugin) as *const libc::c_void;
+    unsafe {
+        jack_set_process_callback(p.client, process, cbpluginptr);
+        jack_activate(p.client);
+        let five = Duration::new(5, 0);
+        loop {
+            std::thread::sleep(five);
+        }
+        // jack_port_unregister(p.client, p.in_port.handle);
+        // jack_port_unregister(p.client, p.output.handle);
+        // jack_client_close(p.client);
+    }
 }
